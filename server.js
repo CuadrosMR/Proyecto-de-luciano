d6b004e04e246e27d3c22c3ca4925c2c7a7b582c
Snoop <Jage820703@gmail.com>
2025-05-13 11:12:02 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,137 +1,54 @@
-const express = require('express');
+const express = required('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
-const app = express();
-
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
-
-app.use(bodyParser.urlencoded({ extended: false }));
+const app= express();
+app.use(bodyParser.urlencoded({ extended:false}));
 app.use(express.static('public'));
-
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'deportes' 
+app.set('view engine', 'ejs');
+const db = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'familia'
 });
-
-connection.connect((err) => {
-  if (err) throw err;
-  console.log('Conectado a MySQL');
+db.connect(err =>{
+    if(err) throw err;
+    console.log('Conectado a mysql');
 });
-
-app.get('/', (req, res) => {
-  const sql = 'SELECT * FROM usuarios1';
-  connection.query(sql, (err, resultados) => {
-    if (err) throw err;
-    res.render('index', { datos: resultados });
-  });
+app.get('/', (req, res)=>{
+    db.query('SELECT * FROM datos_familiares', (err,results)=>{
+        if (err) throw err;
+        res.render('index', {datos: results});
+    });
 });
-
-app.post('/guardar', (req, res) => {
-  const datos = req.body;
-  const sql = `
-    INSERT INTO usuarios1 
-    (nombre, apellidoPaterno, apellidoMaterno, fechaDeNacimiento, problemasDeSalud, domicilio, numeroCelular, email, sexo)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
-  `;
-  const valores = [
-    datos.nombre,
-    datos.apellidoPaterno,
-    datos.apellidoMaterno,
-    datos.fechaDeNacimiento,
-    datos.problemasDeSalud,
-    datos.domicilio,
-    datos.numeroCelular,
-    datos.email,
-    datos.sexo
-  ];
-  connection.query(sql, valores, (err) => {
-    if (err) throw err;
-    res.redirect('/');
-  });
+app.post('/add', (req,res)=>{
+    const { ganancias, gastos, miembros } = req.body;
+    db.query(
+        'INSERT INTO datos_familiares (ganancias, gastos, miembros_trabajando) VALUES(?,?,?)',
+        [ganancias, gastos, miembros],
+        err=>{
+            if (err) throw err;
+            res.redirect('/');
+        }
+    );
 });
-//consultar
-app.get('/usuario/:id', (req, res) => {
-  const id = req.params.id;
-  const sql = 'SELECT * FROM usuarios1 WHERE idusuario = ?';
-
-  connection.query(sql, [id], (err, resultados) => {
-    if (err) throw err;
-
-    if (resultados.length === 0) {
-      return res.status(404).send('Usuario no encontrado');
-    }
-
-    res.render('detalle', { usuario: resultados[0] });
-  });
+app.post('/update/:id', (req, res)=>{
+    const { ganancias, gastos, miembros} =req.body;
+    db.query(
+        'UPDATE datos_familiares SET ganancias =?, gastos = ?, miembros_trabajando = ? WHERE id = ?',
+        [ganancias, gastos, miembros, req.params.id],
+        err =>{
+            if(err) throw err;
+            res.redirect('/');
+        }
+    );
 });
-//actualizar
-app.get('/editar/:id', (req, res) => {
-  const id = req.params.id;
-  const sql = 'SELECT * FROM usuarios1 WHERE idusuario = ?';
-
-  connection.query(sql, [id], (err, resultados) => {
-    if (err) throw err;
-
-    if (resultados.length === 0) {
-      return res.status(404).send('Usuario no encontrado');
-    }
-
-    res.render('editar', { usuario: resultados[0] });
-  });
+app.post('delete/:id', (req,res)=>{
+    db.query('DELETE FROM datos_familiares WHERE id = ?', [req.params.id], err =>{
+        if (err) throw err;
+        res.redirect('/');
+    });
 });
-
-// Guardar cambios del usuario editado
-app.post('/actualizar/:id', (req, res) => {
-  const id = req.params.id;
-  const datos = req.body;
-
-  const sql = `
-    UPDATE usuarios1 SET 
-      nombre = ?, 
-      apellidoPaterno = ?, 
-      apellidoMaterno = ?, 
-      fechaDeNacimiento = ?, 
-      problemasDeSalud = ?, 
-      domicilio = ?, 
-      numeroCelular = ?, 
-      email = ?, 
-      sexo = ?
-    WHERE idusuario = ?
-  `;
-
-  const valores = [
-    datos.nombre,
-    datos.apellidoPaterno,
-    datos.apellidoMaterno,
-    datos.fechaDeNacimiento,
-    datos.problemasDeSalud,
-    datos.domicilio,
-    datos.numeroCelular,
-    datos.email,
-    datos.sexo,
-    id
-  ];
-
-  connection.query(sql, valores, (err) => {
-    if (err) throw err;
-    res.redirect('/');
-  });
-});
-
-// Eliminar usuario
-app.get('/eliminar/:id', (req, res) => {
-  const id = req.params.id;
-  const sql = 'DELETE FROM usuarios1 WHERE idusuario = ?';
-
-  connection.query(sql, [id], (err) => {
-    if (err) throw err;
-    res.redirect('/');
-  });
-});
-
-app.listen(4000, () => {
-  console.log('Servidor iniciado en http://localhost:4000');
+app.listen(8000, ()=>{
+    console.log('Servidor en http://localhost:8000');
 });
